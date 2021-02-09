23e050a03da8b0ae0547f21cea78e5a6c8b2e985
brunomotadev <brunomotadev@gmail.com>
2021-02-08 23:35:16 -0300

    teste java alura

---
+++ TestaVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 29;
+		
+		System.out.println(idade);
+		
+		idade = 19 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) - 6;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + ", parabéns!");
+	}
+}
