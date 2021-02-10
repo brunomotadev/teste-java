2bf6c9fe1b82720dda9a0c0214c9d0f38f1734ca
brunomotadev <brunomotadev@gmail.com>
2021-02-09 22:58:54 -0300

    multiplos de 3

---
+++ MultiplosDe3.java
@@ -0,0 +1,19 @@
+
+public class MultiplosDe3 {
+	
+	public static void main(String[] args) {
+		for(int contador = 0; contador <= 100; contador++)
+			if(contador % 3 == 0) {
+			System.out.println(contador);
+		}
+	}
+}
+
+
+// outra opção
+
+//public static void main (String[] args) {
+//    for (int i = 3; i < 100; i += 3 ){
+//        System.out.println(i);
+//    }
+//}
