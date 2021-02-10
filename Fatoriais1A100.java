4d4e21e376ea571287fbd576d0b0271542151a2c
brunomotadev <brunomotadev@gmail.com>
2021-02-09 23:07:18 -0300

    fatorial 1 a 10

---
+++ Fatoriais1A100.java
@@ -0,0 +1,12 @@
+
+public class Fatoriais1A100 {
+	
+	public static void main(String[] args) {
+		int fatorial = 1;
+		
+		for(int n = 1; n <= 10; n++) {
+			fatorial *= n;
+			System.out.println("Fatorial de " + n + " = " + fatorial);
+		}
+	}
+}
